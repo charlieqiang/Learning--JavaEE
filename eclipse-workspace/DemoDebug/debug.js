ff7730eb673ecc47b0ecc90cc6728cf3bf38ff78
chenlinqiang <327371999@qq.com>
2020-02-13 15:32:02 +0800

    Update debug.js

--- eclipse-workspace/DemoDebug/debug.js
+++ eclipse-workspace/DemoDebug/debug.js
@@ -1,23 +1,59 @@
 /**
+ * 1
+ */
+//console.log('hello!');
+//var n = window.prompt("plz input an int");
+//n = parseInt(n);
+//var res = 0;
+//for(var i=1;i<=n;i++){
+//	res+=i;
+//}
+//document.writeln("res="+res);
+//
+//var res2=0;
+//var temp=1;
+//
+//for(var i=1;i<=n;i++){
+//	for(var j=1;j<=i;j++){
+//		temp+=j;
+//	}
+//	res2+=temp;
+//	temp=1;
+//}
+//document.writeln("res2="+res);
+/**
+ * 2
+ */
+//function Person() {
+//	this.name="abc";
+//	this.abc=function(){
+//		window.alert(this.name);
+//	}
+//
+//}
+//
+//var p1=new Person();
+//p1.name="cde";
+//p1.abc();
+//
+//var p2=new Person();
+//p2.abc();
+/**
+ * 3
  * 
  */
-console.log('hello!');
-var n = window.prompt("plz input an int");
-n = parseInt(n);
-var res = 0;
-for(var i=1;i<=n;i++){
-	res+=i;
-}
-document.writeln("res="+res);
+var arr=new Array(3);
+arr[0]="Geo";
+arr[1]="Joh";
+arr[2]="Cha";
 
-var res2=0;
-var temp=1;
-
-for(var i=1;i<=n;i++){
-	for(var j=1;j<=i;j++){
-		temp+=j;
+Array.prototype.find=function(val){
+	for(var i=0;i<this.length;i++){
+		if(val==this[i]){
+			return i;
+		}
 	}
-	res2+=temp;
-	temp=1;
+	return -1;
 }
-document.writeln("res2="+res);
+
+document.writeln("John index="+arr.find("Joh"));
